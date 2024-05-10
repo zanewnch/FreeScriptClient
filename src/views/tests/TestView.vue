da7fa5a1cbbe6885ae6fcd13f34924c61e0ad307
zanewnch <zanewnch@gmail.com>
2024-05-11 01:47:17 +0800

    temp.

--- src/views/tests/TestView.vue
+++ src/views/tests/TestView.vue
@@ -1,23 +1,9 @@
 <script lang="ts" setup>
-import type { AxiosResponse } from 'node_modules/axios/index.cjs'
-import request from '../../utils/Request'
-import type { Result } from '@/utils/Result'
-import { useGlobalStore } from '../../stores/GlobalStore'
 
-const globalStore = useGlobalStore()
-
-const test = async () => {
-  try {
-    globalStore.test = 'test'
-  } catch (e) {
-    console.log(e)
-  }
-}
 </script>
 
 <template>
-  <button @click="test">submit</button>
-  <div>{{ globalStore.test }}</div>
+  <div>aaa</div>
 </template>
 
 <style scoped lang="scss"></style>
